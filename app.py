0fa59c18f4f1ccca0ab68b400aa861aa5376c425
nabil42info <nabil42.informatique@gmail.com>
2025-04-13 20:49:45 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def serve_file(filename):
     return send_from_directory(".", filename)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=port, debug=True)
