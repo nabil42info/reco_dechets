8cf67d1be4fde440c5ccfe8b16c8b0754bfe53d3
nabil42info <nabil42.informatique@gmail.com>
2025-04-13 20:16:13 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, send_from_directory
 from flask_cors import CORS
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
 
 app = Flask(__name__, static_folder=".")
 CORS(app)  # Active CORS sur toutes les routes
